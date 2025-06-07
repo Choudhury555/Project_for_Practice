5a00e244f4c57022342ec62b4fe8945570f4f5d7
Subash2622 <subash9938sahoo@gmail.com>
2025-06-07 16:09:21 +0530

    Subash Changes

---
+++ src/JAVA_PRACTICE/P2.java
@@ -0,0 +1,9 @@
+public class P2 {
+    public static void main(String[] args) {
+        int num = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+            System.out.println("Hi Abhisek Ji ... Tuma pai gote Russian anichi.");
+        }
+    }
+}
