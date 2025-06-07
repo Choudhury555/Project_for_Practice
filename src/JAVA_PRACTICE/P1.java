8d9399b92f62debd8ae5002af93afd0eecc254b9
Subash2622 <subash9938sahoo@gmail.com>
2025-06-07 17:00:45 +0530

    New change after conflict

--- src/JAVA_PRACTICE/P1.java
+++ src/JAVA_PRACTICE/P1.java
@@ -2,5 +2,6 @@ public class P1 {
     public static void main(String[] args) {
         System.out.println("Ajay");
         System.out.println("Hello, World!");
+        System.out.println("Hello, from Subash");
     }
 }
