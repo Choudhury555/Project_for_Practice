5d960bf7e8373b33077adacb2f4bd4c108bae721
Subash2622 <subash9938sahoo@gmail.com>
2025-06-07 16:35:11 +0530

    Conflict clear

--- src/JAVA_PRACTICE/P1.java
+++ src/JAVA_PRACTICE/P1.java
@@ -1,6 +1,5 @@
 public class P1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello Ajaya Bhai Java King");
+        System.out.println("Hello, World!");
     }
 }
